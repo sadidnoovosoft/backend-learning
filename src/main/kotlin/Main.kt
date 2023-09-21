0b8bb12f0ed28a52ba5c7156c84eb3f6c21300a1
sadidnoovosoft <sadid.shaikh@noovosoft.com>
2023-09-21 18:30:09 +0530

    Type casting and type checking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,52 +1,22 @@
+fun getStringLength(obj: String?): Int? {
+    if (obj is String && obj.isNotEmpty()) {
+        return obj.length
+    }
+    return null
+}
+
+fun getExpressionLength(obj: Any?): Int? {
+    when(obj) {
+        is String -> return obj.length
+        else -> return obj.toString().length
+    }
+}
+
 fun main() {
-    var internsArray = arrayOf("sadid", "tejas", "pulkit")
-    println(internsArray.joinToString())
-    internsArray += "vaishnav"
-    println(internsArray.joinToString())
+    println(getStringLength(""))
+    println(getStringLength(null))
 
-    val nullArray: Array<String?> = arrayOfNulls(5)
-    println(nullArray[0])
-    println(nullArray.joinToString(","))
-
-    val arr = Array<Int>(3) { i -> i + 1 }
-    for (item in arr) {
-        print("$item  ")
-    }
-    println()
-
-    // Multi-dimensional arrays
-    val arr1 = Array(3) { i -> Array<Int>(3) { j -> i + j } }
-    println(arr1.contentDeepToString())
-
-    for (array in arr1) {
-        for (value in array) {
-            print("$value ")
-        }
-        println()
-    }
-    println()
-
-    arr1[0][0] = -99
-    for(i in arr1.indices) {
-        for(j in arr1[i].indices) {
-            print("${arr1[i][j]} ")
-        }
-        println()
-    }
-    println()
-
-    val simpleArray = arrayOf(1, 2, 3)
-    val anotherArray = arrayOf(1, 2, 3)
-
-    println(simpleArray contentDeepEquals  anotherArray)
-
-
-    // Functions on arrays
-    val sumArray = arrayOf(1, 7, -16)
-    println(sumArray.javaClass.name)
-
-    println(sumArray.sum())
-
-    sumArray.shuffle()
-    println(sumArray.joinToString())
+    println(getExpressionLength(123))
+    println(getExpressionLength(true))
+    println(getExpressionLength(null))
 }
