751755922109b7b1162445abe0d54a766660d54c
sadidnoovosoft <sadid.shaikh@noovosoft.com>
2023-09-21 14:47:09 +0530

    Compare difference between ==, ===, .equals()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    data class Employee (val name: String)
+//    class Employee(val name: String)
+
+    val emp1 = Employee("Sadid")
+    val emp2 = Employee("Sadid")
+
+    println(emp1 == emp2) // in case of data class only data will be compared
+    println(emp1 === emp2)
+    println(emp1.equals(emp2)) // in case of data class only data will be compared
+
+    println()
+
+    println(emp1.name == emp2.name)
+    println(emp1.name === emp2.name)
+    println(emp1.name.equals(emp2.name))
 }
